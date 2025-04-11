9f89045c7af7ab87d8c532e971a9f7c5cbb3829f
maxmzts <maximomzts@gmail.com>
2025-04-11 20:17:06 +0200

    VFXManager es singleton y acepta invertir horizontalmente el efecto

--- src/pro/inNominePatris/managers/VFXManager.h
+++ src/pro/inNominePatris/managers/VFXManager.h
@@ -5,21 +5,33 @@
 #include <memory>
 #include "VisualEffect.h"
 
+/**
+ * Manager planteado para gestionar los efectos visuales del juego.
+ * Se plantea como un singleton para que se gestione todo el VFX en
+ * un único lugar pero que de igual manera se pueda acceder desde 
+ * distintos sitios.
+ * Su diseño depende directamente del diseño de "VisualEffect".
+ */
 class VFXManager {
 private:
     std::vector<std::unique_ptr<VisualEffect>> effects;
-public:
     VFXManager() = default;
+    // No copy
+    VFXManager(const VFXManager&) = delete;
+    VFXManager& operator=(const VFXManager&) = delete;
+public:
+    static VFXManager& getInstance();
 
     void addEffect(
         const std::string& texturePath,
         sf::Vector2f position,
         sf::Vector2i frameSize,
         int frameCount,
-        float frameDuration
+        float frameDuration,
+        bool invert = false
     );
 
     void update(float deltaTime);
-    void draw(sf::RenderWindow& window);
+    void render(sf::RenderWindow& window);
 };
 
