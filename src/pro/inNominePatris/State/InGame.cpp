5f28fe4e9e08cd5a2e694fec621f63753a1006d7
maxmzts <maximomzts@gmail.com>
2025-04-14 15:56:33 +0200

    Arreglado problema con el arco

--- src/pro/inNominePatris/State/InGame.cpp
+++ src/pro/inNominePatris/State/InGame.cpp
@@ -283,12 +283,6 @@ void InGame::update(Game& game) {
         }
     }
 
-    // Comprobar que algún enemigo recibe daño
-    for(auto enemy : EnemyManager::getInstance()->getEnemyList()){
-        if(enemy->getisInvincible() && checkEnemyWasHit(enemy, player))
-            enemy->takeDamage(player.getEquippedWeapon()->calculateDamage(),  player.getEquippedWeapon()->getAttackHitbox()->getPosition());
-    }
-
     hud.update(player);
 
     VFXManager::getInstance().update(deltaTime);
@@ -356,6 +350,7 @@ void InGame::render(Game& game, sf::RenderWindow& window) {
     engine.display();
 }
 
+//  QUITAR
 bool InGame::checkEnemyWasHit(std::shared_ptr<Enemy> enemy, Character player){
     if(player.getEquippedWeapon()->getAttackHitbox()->isActive())
         return enemy->getHurtbox()->getGlobalBounds().intersects(player.getEquippedWeapon()->getAttackHitbox()->getGlobalBounds());
